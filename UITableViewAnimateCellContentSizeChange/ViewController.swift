b0404053682783895c8d0495b11154879d120557
רותם פלאפון <roeyhonig94@gmail.com>
2019-07-13 17:18:53 +0300

    Initial Commit

---
+++ UITableViewAnimateCellContentSizeChange/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UITableViewAnimateCellContentSizeChange
+//
+//  Created by Rotem walzer on 13.7.2019.
+//  Copyright © 2019 Rotem walzer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ UITableViewAnimateCellContentSizeChangeTests/UITableViewAnimateCellContentSizeChangeTests.swift
@@ -0,0 +1,34 @@
+//
+//  UITableViewAnimateCellContentSizeChangeTests.swift
+//  UITableViewAnimateCellContentSizeChangeTests
+//
+//  Created by Rotem walzer on 13.7.2019.
+//  Copyright © 2019 Rotem walzer. All rights reserved.
+//
+
+import XCTest
+@testable import UITableViewAnimateCellContentSizeChange
+
+class UITableViewAnimateCellContentSizeChangeTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
